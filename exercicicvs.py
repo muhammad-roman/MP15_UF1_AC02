4c53eef3ad60cc05a224a0070c4002cf3e1b27ce
origin <muhammad-roman.aziz.7e5@itb.cat>
2023-10-08 12:58:30 +0200

    PRTE UNO CREO K EST

--- exercicicvs.py
+++ exercicicvs.py
@@ -1,17 +1,17 @@
-
-# extract
-
 import csv
 
+def llegir_dades(nom_fitxer):
+    dades = []
+    with open(nom_fitxer, 'r') as file:
+        csv_reader = csv.DictReader(file, delimiter=';')
+        for row in csv_reader:
+            dades.append(row)
+    return dades
 
-csv_filename = 'basket_players.csv'
+dades = llegir_dades('basket_players.csv')
 
-with open(csv_filename) as f:
-    reader = csv.DictReader(f)
-
-    for row in reader:
-        print(row)
-
-
-# transform
-
+for i, jugador in enumerate(dades):
+    print(f"Jugador {i}:")
+    for camp, valor in jugador.items():
+        print(f"{camp}: {valor}")
+    print("=" * 20)
